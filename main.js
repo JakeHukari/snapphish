183836adca7fd424b21fbc773d289dab8f83340a
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-04-24 10:27:18 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const fsLibrary  = require('fs') 
+import { writeFile } from 'fs'; 
 
 function logIn() {  
 // Data which will need to add in a file. 
 let data = "Hello world 2."
   
 // Write data in 'newfile.txt' . 
-fsLibrary.writeFile('data.txt', data, (error) => { 
+    writeFile('data.txt', data, (error) => { 
       
     // In case of a error throw err exception. 
     if (error) throw err; 
