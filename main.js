7b75b68e09b5d54315c87c08cc6ec39b92bd2faf
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-04-24 10:42:37 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 import { writeFile } from 'fs'; 
 
 function logIn() {  
-// Data which will need to add in a file. 
 let data = "Hello world 2."
-  
-// Write data in 'newfile.txt' . 
+   
     writeFile('data.txt', data, (error) => { 
-      
-    // In case of a error throw err exception. 
+       
     if (error) throw err; 
 }) 
 }
