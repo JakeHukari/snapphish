4e83acae877eec5d60f01d83f0c661489b6ff8ab
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-04-23 23:46:30 -0600

    update

--- writefile test/main.js
+++ writefile test/main.js
@@ -1,5 +1,11 @@
-window.onload = writeFile;
-
-function writeFile() {
-    writeFile()
-}
+const fsLibrary  = require('fs') 
+  
+// Data which will need to add in a file. 
+let data = "Hello world 2."
+  
+// Write data in 'newfile.txt' . 
+fsLibrary.writeFile('data.txt', data, (error) => { 
+      
+    // In case of a error throw err exception. 
+    if (error) throw err; 
+}) 
